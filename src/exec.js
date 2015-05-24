18e595a70e820e6235d0364b345ba3bcf15db333
Andrin Bertschi <andrin.bertschi@gmail.com>
2015-05-24 02:24:28 +0200

    introduce exec.js

---
+++ src/exec.js
@@ -0,0 +1,64 @@
+var temp  = require('temp'),
+    fs    = require('fs'),
+    util  = require('util'),
+    path  = require('path'),
+    GulpSsh = require('gulp-ssh');
+
+
+var Reloadfile = require('./reloadfile').ReloadFile;
+var AuthMethods = require('./reloadfile').AuthMethods;
+
+var _clientMountDir = {};
+
+var ExecUtil = {
+
+    _ssh: null,
+
+    initSsh: function() {
+
+        if (Reloadfile.supportsExec()) {
+            var config = Reloadfile.config;
+
+            var sshConfig = {
+                host: config.device.host,
+                port: config.device.port,
+                username: config.run.user
+            };
+
+            switch (Reloadfile.getAuthModeRun()) {
+                case AuthMethods.KEYFILE:
+                    sshConfig.privateKey = config.run.keyfile;
+                    break;
+                case AuthMethods.USERNAME_PASSWORD:
+                    sshConfig.password = config.run.password;
+                    break;
+                case AuthMethods.PUBLIC_KEY:
+                    break;
+                default:
+            }
+
+            this._ssh = new GulpSsh({
+                ignoreErrors: false,
+                sshConfig: sshConfig
+            });
+        }
+        return this._ssh;
+    },
+
+    execWith: function(cmds) {
+
+        this._ssh.exec(cmds);
+    },
+
+    exec: function() {
+        this.execWith(Reloadfile.config.run.exec);
+    },
+
+    getClientMountDir: function() {
+        return _clientMountDir;
+    }
+}
+
+exports.ExecUtil = ExecUtil;
+
+
